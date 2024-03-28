1bbd700dad1ec33e225acca87e86363c24bf60d7
armaan <adhanji8@gmail.com>
2024-03-28 08:50:15 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const app = express();
+const session = require("express-session");
+const path = require("path");
+const ejsLayouts = require("express-ejs-layouts");
+const reminderController = require("./controller/reminder_controller");
+const authController = require("./controller/auth_controller");
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+      maxAge: 24 * 60 * 60 * 1000,
+    },
+  })
+);
+
+const passport = require("./middleware/passport");
+app.use(express.urlencoded({ extended: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(ejsLayouts);
+
+app.set("view engine", "ejs");
+
+app.post(
+  "/auth/login",
+  passport.authenticate("local", {
+    successRedirect: "/reminders",
+    failureRedirect: "/auth/login",
+  })
+);
+
+// Routes start here
+app.get("/reminders", reminderController.list);
+app.get("/reminder/new", reminderController.new);
+app.get("/reminder/:id", reminderController.listOne);
+app.get("/reminder/:id/edit", reminderController.edit);
+app.post("/reminder/", reminderController.create);
+// ⭐ Implement these two routes below!
+app.post("/reminder/update/:id", reminderController.update);
+app.post("/reminder/delete/:id", reminderController.delete);
+
+// 👌 Ignore for now
+app.get("/register", authController.register);
+app.get("/login", authController.login);
+app.post("/register", authController.registerSubmit);
+app.post("/login", authController.loginSubmit);
+
+app.listen(3001, function () {
+  console.log(
+    "Server running. Visit: http://localhost:3001/reminders in your browser 🚀"
+  );
+});
